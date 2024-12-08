346e17528f8002127b596d98d7d3bff280ec2b9c
qwk <80052118+qwkdev@users.noreply.github.com>
2024-12-08 17:11:18 +0000

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,9 @@
 export default async function handler(req, res) {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, X-Target-Path');
+	if (req.method === 'OPTIONS') { return res.status(200).end(); }
+	
 	const targetPath = req.headers['Path'] || 'init';
 	const targetUrl = `https://xrce.pythonanywhere.com/${targetPath}`;
 	const fetchOptions = {
