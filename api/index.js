584d477c66889946d7a5d2e4d88c184a772ebef5
qwk <80052118+qwkdev@users.noreply.github.com>
2024-12-08 17:05:41 +0000

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 export default async function handler(req, res) {
-	const targetUrl = `https://xrce.pythonanywhere.com/init`;
+	const targetPath = req.headers['Path'] || 'init';
+	const targetUrl = `https://xrce.pythonanywhere.com/${targetPath}`;
 	const fetchOptions = {
 		method: req.method,
 		headers: {
