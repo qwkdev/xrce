4d5b935b7e894782e98857d5ac4fcaa37dd06b45
qwk <80052118+qwkdev@users.noreply.github.com>
2024-12-14 21:58:01 +0000

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
 	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Path, Name, Domain');
+	res.setHeader('Access-Control-Allow-Headers', '*');
 	if (req.method === 'OPTIONS') { return res.status(200).end(); }
 
 	const targetPath = req.headers['path'] || 'init';
