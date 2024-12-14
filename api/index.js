8447f9e616668a2a2f2aa02f750e6c68ce1fcdbd
qwk <80052118+qwkdev@users.noreply.github.com>
2024-12-14 15:43:41 +0000

    Update index.js

--- api/index.js
+++ api/index.js
@@ -9,8 +9,9 @@ export default async function handler(req, res) {
 	if (targetPath === 'delete-cookie') {
 		const cookieName = req.headers['name'];
 		if (!cookieName) { return res.status(400).send("Cookie name is required."); }
-		
-		res.setHeader('Set-Cookie', `${cookieName}=; Path=/; HttpOnly; Secure; SameSite=Strict; Expires=Thu, 01 Jan 1970 00:00:00 GMT; Domain=${req.headers['name']}`);
+
+		if (req.headers['domain']) { res.setHeader('Set-Cookie', `${cookieName}=; Path=/; HttpOnly; Secure; SameSite=Strict; Expires=Thu, 01 Jan 1970 00:00:00 GMT; Domain=${req.headers['domain']};`); }
+		else { res.setHeader('Set-Cookie', `${cookieName}=; Path=/; HttpOnly; Secure; SameSite=Strict; Expires=Thu, 01 Jan 1970 00:00:00 GMT;`); }
 		return res.status(200).send(`Cookie "${cookieName}" deleted.`);
 	}
 	
